ffcebbdde8e3af40e6917b301998c34c9e7f06ef
Sandro Miccoli <smiccoli@gmail.com>
2015-05-11 18:52:17 -0300

    SortHand and randomHand functions.
    Functions for hand sorting and randomisation.

--- src/Player.java
+++ src/Player.java
@@ -1,4 +1,6 @@
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 
 
 public class Player {
@@ -14,10 +16,40 @@ public class Player {
 			hand.add(c);
 	}
 	
+	public void removeCard(Card c){
+		if (hand.size()>0)
+			hand.remove(c);
+	}
+	
 	public void printHand(){
 		System.out.println("Current hand...");
 		for (Card c : hand){
 			c.printCard();
 		}
+	}
+
+	public void sortHandBySuit(){
+		Collections.sort(hand, new Comparator<Card>() {
+	        @Override
+			public int compare(Card c1, Card c2){
+	            return  c1.getSuit().compareTo(c2.getSuit());
+	        }
+	    });
+	}
+	
+	public void sortHandByRank(){
+		Collections.sort(hand, new Comparator<Card>() {
+	        @Override
+			public int compare(Card c1, Card c2){
+	            return  c1.getRank().compareTo(c2.getRank());
+	        }
+	    });
+	}
+	
+	public ArrayList<Card> randomHand(){
+		int fromIndex = (int) (Math.random()*7);
+		int toIndex = 1+fromIndex+(int) (Math.random()*5);
+		
+		return new ArrayList<Card>(hand.subList(fromIndex, toIndex));
 	}
 }
