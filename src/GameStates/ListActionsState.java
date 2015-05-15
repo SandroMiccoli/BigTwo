b83307bddb498936630fd422a4721aefb2a7a2e0
Sandro Miccoli <smiccoli@gmail.com>
2015-05-15 14:05:07 -0300

    Re-ordering actions for consistency

--- src/GameStates/ListActionsState.java
+++ src/GameStates/ListActionsState.java
@@ -32,29 +32,29 @@ public class ListActionsState implements GameState {
 	}
 	
 	public void listActions(){
-		System.out.println("1) Pass");
-		System.out.println("2) Sort hand by suit");
-		System.out.println("3) Sort hand by rank");
-		System.out.println("4) Create combination of cards");
+		System.out.println("1) Sort hand by suit");
+		System.out.println("2) Sort hand by rank");
+		System.out.println("3) Create combination of cards");
+		System.out.println("4) Pass");
 	}
 	
 	public GameState nextState(int action, BigTwo game){
 
 		switch(action){
 			case 1:
-				return new NextPlayerState();
-			case 2:
 				System.out.println("Sorting hand by suit...");
 				game.getCurrentPlayer().sortHandBySuit();
 				game.getCurrentPlayer().printHand();
 				return new ListActionsState();
-			case 3:
+			case 2:
 				System.out.println("Sorting hand by rank...");
 				game.getCurrentPlayer().sortHandByRank();
 				game.getCurrentPlayer().printHand();
 				return new ListActionsState();
-			case 4:
+			case 3:
 				return new CreateCombinationState();
+			case 4:
+				return new NextPlayerState();
 			default:
 				System.out.println("Invalid option...");
 				return new ListActionsState();
